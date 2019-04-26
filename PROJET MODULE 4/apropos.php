81faa2b183e9e643c22399a02e172eadeb427eca
Itachy-Utiwa <47219315+Itachy-Utiwa@users.noreply.github.com>
2019-04-26 13:10:58 +0000

    Add files via upload

---
+++ PROJET MODULE 4/apropos.php
@@ -0,0 +1,21 @@
+<div class="w3-container-fluid">
+
+<?php include('header.php')?>
+
+<div class="w3-padding-32">
+</div>
+<div class="w3-padding-32">
+</div>
+
+<div class="w3-center">
+    <img src="images/logo.png" style="width:15%">
+    <h3 class="w3-text-white">E-Building,</h3>
+    <div class="w3-text-black ">
+      
+      <p class="w3-center"><b>Une solution innovente dans le domaine de la construction sur tout l'étendu du territoire ivoirien</b></p>    
+    </div>
+  </div>
+
+
+  <?php include('footer.php')?>
+</div>
---
+++ PROJET MODULE 4/index.php
@@ -0,0 +1,39 @@
+<?php include('header.php');?>
+
+<div class="w3-container w3-padding-16">                            
+</div>
+
+<div class="w3-container w3-padding-64 w3-center w3-animate-fading ">
+<h3 style="color:white">Bienvenue sur E-Building, le site de location, de vente d'appartement et de construction de maison en ligne</h3>                            
+</div>
+
+<div class="w3-container- w3-center row">
+<div class="w3-container col-md-6">
+<a href="location.php" class="w3-black w3-button w3-padding-large w3-hover-orange w3-round-xxlarge" style="text-decoration:none">RECHERCHER UN LOCAL</a>                            
+</div>
+<div class="w3-container col-md-6">
+<a href="vente.php" class="w3-black col-md-6 w3-button w3-padding-large w3-hover-orange w3-round-xxlarge" style="text-decoration:none">VENDRE</a>                            
+</div>
+</div>
+<br>
+
+<div class="w3-container col-md-12">
+<a href="https://www.archifacile.fr/planenligne" class="w3-orange col-md-12 w3-button w3-padding-large w3-hover-black w3-round-xxlarge" style="text-decoration:none">CONSTRUIRE, MODELISER VOTRE MAISON A L'INSTANT</a>                            
+</div>
+</div>
+
+
+<div class="w3-container w3-padding-64">                            
+</div>
+
+<div class="w3-container w3-padding-64 w3-center w3-animate-fading ">
+<h3 style="color:white">Bienvenue sur E-Building, le site de location, de vente d'appartement et de construction de maison en ligne</h3>                            
+</div>
+
+
+
+
+<div class="w3-container w3-padding-64">                            
+</div>
+
+<?php include('footer.php')?>
